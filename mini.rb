000a875f0af10163d845dc3601aa4f9b006056db
Guilherme Ceolin <guiceolin@gmail.com>
2016-08-15 22:08:42 -0300

    make trailing slash optional

--- mini.rb
+++ mini.rb
@@ -4,22 +4,22 @@ class Mini < Sinatra::Base
     "Welcome to Mini!"
   end
 
-  get "/urls/new" do
+  get "/urls/new/?" do
     erb :new
   end
 
-  post "/urls/" do
+  post "/urls/?" do
     url = Url.create(original: params[:original])
     short = Bijective.encode(url.id)
     redirect to "/#{short}/info"
   end
 
-  get "/:short/info" do
+  get "/:short/info/?" do
     url = Url[Bijective.decode(params[:short])]
     erb :info, locals: { url: url }
   end
 
-  get "/:short" do
+  get "/:short/?" do
     url = Url[Bijective.decode(params[:short])]
     url.count += 1
     url.save_changes
