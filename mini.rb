2206ef0c5da7dc50200f94355034a4c0a87b74ff
Guilherme Ceolin <guiceolin@gmail.com>
2016-08-15 22:01:20 -0300

    Simple views

--- mini.rb
+++ mini.rb
@@ -1,8 +1,29 @@
-require 'sinatra/base'
-
 class Mini < Sinatra::Base
 
   get "/" do
-    "Hello World"
+    "Welcome to Mini!"
   end
+
+  get "/urls/new" do
+    erb :new
+  end
+
+  post "/urls/" do
+    url = Url.create(original: params[:original])
+    short = Bijective.encode(url.id)
+    redirect to "/#{short}/info"
+  end
+
+  get "/:short/info" do
+    url = Url[Bijective.decode(params[:short])]
+    erb :info, locals: { url: url }
+  end
+
+  get "/:short" do
+    url = Url[Bijective.decode(params[:short])]
+    url.count += 1
+    url.save_changes
+    redirect url.original
+  end
+
 end
